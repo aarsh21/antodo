be6044dc7303358ea24f6332faf91c2589a98065
aarsh21 <aarshpersonal21@gmail.com>
2023-05-29 18:49:55 +0530

    implemented helper function
    made a helper function getTodosGroupedByColumn to use Map ds and fetched value from database and sorted it.

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,18 +1,22 @@
 "use client";
+import { useBoardStore } from "@/store/BoardStore";
 import React, { useEffect } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 function Board() {
+  const getBoard = useBoardStore((state) => state.getBoard);
+
   useEffect(() => {
-    
-  }, []);
+    getBoard();
+  }, [getBoard]);
 
   return (
-    <DragDropContext>
-      <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided) => <div>{/* rendering columns */}</div>}
-      </Droppable>
-    </DragDropContext>
+    <h1></h1>
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal" type="column">
+    //     {(provided) => <div>{/* rendering columns */}</div>}
+    //   </Droppable>
+    // </DragDropContext>
   );
 }
 
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,3 +1,4 @@
+import { getTodosGroupedByColumn } from "@/lib/getTodosGroupedByColumns";
 import { create } from "zustand";
 
 interface BoardState {
@@ -5,7 +6,7 @@ interface BoardState {
   getBoard: () => void;
 }
 
-const useBoardStore = create<BoardState>((set) => ({
+export const useBoardStore = create<BoardState>((set) => ({
   board: {
     coloums: new Map<TypedColumn, Column>(),
   },
