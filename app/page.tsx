29d545b8293aeced14509b0ac27fdf14ace29086
aarsh21 <aarshpersonal21@gmail.com>
2023-05-29 17:03:06 +0530

    Add Header Component
    added header with sleekUi and Search Form

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import Image from 'next/image'
+import { headers } from "next/dist/client/components/headers";
+import React from "react";
+import { BugAntIcon } from "@heroicons/react/24/outline";
+import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import Avatar from "react-avatar";
+import Header from "@/components/Header";
+import Board from "@/components/Board";
 
-export default function Home() {
+function page() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      
-    </main>
-  )
+    <>
+      <Header />
+      <Board />
+    </>
+  );
 }
+
+export default page;
