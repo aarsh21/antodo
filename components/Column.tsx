222963d741093b4c146b8a5ac9b1ef4db9f24c39
aarsh21 <aarshpersonal21@gmail.com>
2023-05-30 18:13:03 +0530

    Add : Search Funtionality
    added search Funtionality using global funtion which we set up .

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,6 +1,7 @@
 import { PlusCircleIcon } from "@heroicons/react/24/solid";
 import { Draggable, Droppable } from "react-beautiful-dnd";
 import TodoCard from "./TodoCard";
+import { useBoardStore } from "@/store/BoardStore";
 
 type props = {
   id: TypedColumn;
@@ -17,6 +18,8 @@ const idToColumnText: {
 };
 
 function Column({ id, todos, index }: props) {
+  const [searchString] = useBoardStore((state) => [state.searchString]);
+
   return (
     <Draggable draggableId={id} index={index}>
       {(provided) => (
@@ -37,37 +40,50 @@ function Column({ id, todos, index }: props) {
                 <h2 className="flex justify-between p-2 text-xl font-bold">
                   {idToColumnText[id]}
                   <span className="px-2 py-1 text-sm font-normal text-gray-500 bg-gray-200 rounded-full">
-                    {todos.length}
+                    {!searchString
+                      ? todos.length
+                      : todos.filter((todo) =>
+                          todo.title
+                            .toLowerCase()
+                            .includes(searchString.toLowerCase())
+                        ).length}
                   </span>
                 </h2>
                 <div className="space-y-2">
-                  {todos.map((todo, index) => (
-                    <Draggable
-                      key={todo.$id}
-                      draggableId={todo.$id}
-                      index={index}
-                    >
-                      {(provided) => (
-                        <TodoCard
-                          todo={todo}
-                          index={index}
-                          id={id}
-                          innerRef={provided.innerRef}
-                          dragHandleProps={provided.dragHandleProps}
-                          draggableProps={provided.draggableProps}
-                        />
-                      )}
-                    </Draggable>
-                  ))}
+                  {todos.map((todo, index) => {
+                    if (
+                      searchString && 
+                      !todo.title
+                        .toLowerCase()
+                        .includes(searchString.toLowerCase())
+                    )
+                      return null;
+                    return (
+                      <Draggable
+                        key={todo.$id}
+                        draggableId={todo.$id}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <TodoCard
+                            todo={todo}
+                            index={index}
+                            id={id}
+                            innerRef={provided.innerRef}
+                            dragHandleProps={provided.dragHandleProps}
+                            draggableProps={provided.draggableProps}
+                          />
+                        )}
+                      </Draggable>
+                    );
+                  })}
                   {provided.placeholder}
-                   
-                   <div className="flex items-end justify-end p-2">
+
+                  <div className="flex items-end justify-end p-2">
                     <button className="text-green-500 hover:text-green-600">
-                        <PlusCircleIcon
-                        className="w-10 h-10 "
-                        />
+                      <PlusCircleIcon className="w-10 h-10 " />
                     </button>
-                   </div>
+                  </div>
                 </div>
               </div>
             )}
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,46 +1,55 @@
-"use client"
-import { BugAntIcon, MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/outline'
-import React from 'react'
-import Avatar from 'react-avatar'
+"use client";
+import { useBoardStore } from "@/store/BoardStore";
+import {
+  BugAntIcon,
+  MagnifyingGlassIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/outline";
+import React from "react";
+import Avatar from "react-avatar";
 
 function Header() {
+  const [searchString, setSearchString] = useBoardStore((state) => [
+    state.searchString,
+    state.setSearchString,
+  ]);
   return (
     <header>
-    <div className="flex flex-col items-center p-5 px-32 bg-black/20 md:flex-row">
-        <div 
-            className='absolute top-0 left-0 w-full rounded-md opacity-50 h-96 bg-gradient-to-br from-pink-400 to-red-400 filter blur-3xl -z-50' />
-      <div className="flex items-center gap-2 p-4 ">
-        <BugAntIcon className="object-contain h-10 w-14" />
-        <h1 className="text-4xl">AnTodo</h1>
+      <div className="flex flex-col items-center p-5 px-32 bg-black/20 md:flex-row">
+        <div className="absolute top-0 left-0 w-full rounded-md opacity-50 h-96 bg-gradient-to-br from-pink-400 to-red-400 filter blur-3xl -z-50" />
+        <div className="flex items-center gap-2 p-4 ">
+          <BugAntIcon className="object-contain h-10 w-14" />
+          <h1 className="text-4xl">AnTodo</h1>
+        </div>
+        <div className="flex items-center justify-end flex-1 space-x-5 ml-14">
+          {/* search box */}
+          <form className="flex items-center flex-initial p-2 space-x-5 bg-white rounded shadow-md">
+            <MagnifyingGlassIcon className="text-gray-400 w-7 h-7" />
+            <input
+              type="text"
+              placeholder="Search"
+              value={searchString}
+              onChange={(e) => setSearchString(e.target.value)}
+              className="flex-1 p-2 outline-none"
+            />
+            <button type="submit" hidden>
+              Search
+            </button>
+          </form>
+          {/* Avatar */}
+          <Avatar name="Aarsh Padia" round size="50" color="#000000" />
+        </div>
       </div>
-      <div className="flex items-center justify-end flex-1 space-x-5 ml-14">
-        {/* search box */}
-        <form className="flex items-center flex-initial p-2 space-x-5 bg-white rounded shadow-md">
-          <MagnifyingGlassIcon className="text-gray-400 w-7 h-7" />
-          <input
-            type="text"
-            placeholder="Search"
-            className="flex-1 p-2 outline-none"
-          />
-          <button type="submit" hidden>
-            Search
-          </button>
-        </form>
-      {/* Avatar */}
-      <Avatar name='Aarsh Padia' round size='50' color='#000000'/>
-      </div>
-    </div>
-    
 
-    {/* suggestion */}
-    <div className='flex items-center justify-center px-8 py-2 md:py-5'>
-        <p  className='flex items-center mt-4 px-5 p-3 font-light bg-white shadow-xl rounded-xl w-fit italic max-w-3xl text=[#000000]'>
-            <UserCircleIcon className='inline-block w-10 h-10 mr-3 '/>
-            GPT is summarizing your day!
+      {/* suggestion */}
+      <div className="flex items-center justify-center px-8 py-2 md:py-5">
+        <p className="flex items-center mt-4 px-5 p-3 font-light bg-white shadow-xl rounded-xl w-fit italic max-w-3xl text=[#000000]">
+          <UserCircleIcon className="inline-block w-10 h-10 mr-3 " />
+          GPT is summarizing your day!
         </p>
-    </div>
-  </header>
-  )
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
