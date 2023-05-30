d3f815a50ff374b3124404d582ecf996bf674592
aarsh21 <aarshpersonal21@gmail.com>
2023-05-30 17:26:25 +0530

    complete DnD and update Db
    completed DnD functionality and updated the state in database

--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,9 +1,12 @@
+import { databases } from "@/appwrite";
 import { getTodosGroupedByColumn } from "@/lib/getTodosGroupedByColumns";
 import { create } from "zustand";
 
 interface BoardState {
   board: Board;
   getBoard: () => void;
+  setBoardState: (board: Board) => void;
+  updateTodoInDB: (todo: Todo, columns: TypedColumn) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
@@ -13,5 +16,17 @@ export const useBoardStore = create<BoardState>((set) => ({
   getBoard: async () => {
     const board = await getTodosGroupedByColumn();
     set({ board });
+  },
+  setBoardState: (board) => set({ board }),
+  updateTodoInDB: async (todo, columnId) => {
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABSE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id,
+      {
+        title: todo.title,
+        status: columnId,
+      }
+    );
   },
 }));
