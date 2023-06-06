c40ebaa0205a4b9d90d6a50139380efbe47a5707
aarsh21 <aarshpersonal21@gmail.com>
2023-06-06 16:37:31 +0530

    fix Ui

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,10 +19,10 @@ function Header() {
   
   return (
     <header>
-      <div className="flex flex-col items-center p-5 px-32 bg-black/20 md:flex-row">
+      <div className="flex flex-col items-center p-5 px-32 bg-black/10 md:flex-row">
         <div className="absolute top-0 left-0 w-full rounded-md opacity-50 h-96 bg-gradient-to-br from-pink-400 to-red-400 filter blur-3xl -z-50" />
         <div className="flex items-center gap-2 p-4 ">
-          <BugAntIcon className="object-contain h-10 w-14" />
+          <BugAntIcon className="object-contain h-10 p-1 bg-red-600 rounded-lg text-red-50 w-14" />
           <h1 className="text-4xl">AnTodo</h1>
         </div>
         <div className="flex items-center justify-end flex-1 space-x-5 ml-14">
@@ -41,7 +41,7 @@ function Header() {
             </button>
           </form>
           {/* Avatar */}
-          <Avatar name="Aarsh Padia" round size="50" color="#000000" />
+          <Avatar name="Aarsh Padia" round size="50" color="#FF1000" />
         </div>
       </div>
       {/* suggestion */}
